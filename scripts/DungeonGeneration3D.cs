12902fa298dff64f4f4e244ee4bebbb9b27eedcc
itaden1 <ethanshearer1@gmail.com>
2021-08-23 21:47:40 +1000

    there was an attempt at appenFrom

--- scripts/DungeonGeneration3D.cs
+++ scripts/DungeonGeneration3D.cs
@@ -4,7 +4,15 @@ using ProcDungeon.Structures;
 using ProcDungeon.Algorythms;
 using ProcDungeon;
 
-public class DungeonGeneration3D : MeshInstance
+
+public struct UVCoordSet
+{
+    public float x1;
+    public float x2;
+    public float y1;
+    public float y2;
+}
+public class DungeonGeneration3D : Spatial
 {
     [Export]
 	private int _mapSize = 40;
@@ -14,6 +22,11 @@ public class DungeonGeneration3D : MeshInstance
 
     [Export]
     public int TileSize = 2;
+    [Export]
+    public float UVOffset = 0.5F;
+
+    private UVCoordSet _wallUV = new UVCoordSet(){x1=0, y1=0.5F, x2=0.5F, y2=1.0F};
+    private UVCoordSet _floorUV = new UVCoordSet(){x1=0, y1=0, x2=0.5F, y2=0.5F};
 
     private bool _playerSpawned = false;
     public override void _Ready()
@@ -32,30 +45,34 @@ public class DungeonGeneration3D : MeshInstance
 
         SurfaceTool st = new SurfaceTool();
         st.Begin(Mesh.PrimitiveType.Triangles);
-        for (int y = 0; y < myMap.Grid.GetLength(0); y++)
+        // y coord becomes the z coord in 3D space
+        for (int z = 0; z < myMap.Grid.GetLength(0); z++)
         {
             for (int x = 0; x < myMap.Grid.GetLength(1); x++)
             {
-                if (myMap.Grid[y, x].Blocking == false)
+                if (myMap.Grid[z, x].Blocking == false)
                 {
-                    if (myMap.Grid[y, x-1].Blocking)
+                    if (myMap.Grid[z, x-1].Blocking)
                     {
-                        GenerateVerticalPlaneX(st, x, y);
+                        GenerateVerticalPlaneX(st, x, z);
+                        Mesh pillar = (Mesh)ResourceLoader.Load<Mesh>("res://3DPrefabs/pillar.tres");
+                        st.AppendFrom(pillar, 0, new Transform(Basis.Identity, new Vector3(x, 0, z)));
                     }
-                    if (myMap.Grid[y-1, x].Blocking)
+                    if (myMap.Grid[z-1, x].Blocking)
                     {
-                        GenerateVerticalPlaneY(st, x, y);
+                        GenerateVerticalPlaneY(st, x, z);
                     }
-                    if (myMap.Grid[y, x+1].Blocking)
+                    if (myMap.Grid[z, x+1].Blocking)
                     {
-                        GenerateVerticalPlaneX(st, x+1, y);
+                        GenerateVerticalPlaneX(st, x+1, z);
                     }
-                    if (myMap.Grid[y+1, x].Blocking)
+                    if (myMap.Grid[z+1, x].Blocking)
                     {
-                        GenerateVerticalPlaneY(st, x, y+1);
+                        GenerateVerticalPlaneY(st, x, z+1);
                     }
-                    GenerateHorizontalPlane(st, x, y);
-                    if(!_playerSpawned) SpawnPlayer(x, y);
+                    GenerateHorizontalPlane(st, x, z); // floor
+                    GenerateHorizontalPlane(st, x, z, _y: 1); // roof
+                    if(!_playerSpawned) SpawnPlayer(x, z);
                 }
             }
         }
@@ -63,106 +80,110 @@ public class DungeonGeneration3D : MeshInstance
         st.Index();
 
         Mesh mesh = st.Commit();
-        SpatialMaterial material = (SpatialMaterial)ResourceLoader.Load<SpatialMaterial>("res://material/floor_material.tres");
+        SpatialMaterial material = (SpatialMaterial)ResourceLoader.Load<SpatialMaterial>("res://material/maze_material.tres");
         mesh.SurfaceSetMaterial(0, material);
         GD.Print("generation complete");
 
         GD.Print("adding to scene");
-        Mesh = mesh;
-        CreateTrimeshCollision();
+        MeshInstance meshInstance = new MeshInstance();
+        meshInstance.Name = "DungeonMesh";
+        meshInstance.Mesh = mesh;
+        meshInstance.CreateTrimeshCollision();
+        AddChild(meshInstance);
     }
-    private void GenerateVerticalPlaneY(SurfaceTool st, int _x, int _y)
+    private void GenerateVerticalPlaneY(SurfaceTool st, int _x, int _z)
     {
         int x = _x * TileSize;
-        int y = _y * TileSize;
+        int z = _z * TileSize;
 
         // First triangle
         st.AddNormal(new Vector3(0, 0, 1));
-        st.AddUv(new Vector2(x, y));
-        st.AddVertex(new Vector3(x, 0, y));
+        st.AddUv(new Vector2(_wallUV.x1, _wallUV.y2));
+        st.AddVertex(new Vector3(x, 0, z));
 
         st.AddNormal(new Vector3(0, 0, 1));
-        st.AddUv(new Vector2(x, TileSize));
-        st.AddVertex(new Vector3(x, TileSize, y));
+        st.AddUv(new Vector2(_wallUV.x1, _wallUV.y1));
+        st.AddVertex(new Vector3(x, TileSize, z));
 
         st.AddNormal(new Vector3(0, 0, 1));
-        st.AddUv(new Vector2(x, y + TileSize));
-        st.AddVertex(new Vector3(x + TileSize, TileSize, y));
+        st.AddUv(new Vector2(_wallUV.x2, _wallUV.y1));
+        st.AddVertex(new Vector3(x + TileSize, TileSize, z));
 
         // Second triangle
         st.AddNormal(new Vector3(0, 0, 1));
-        st.AddUv(new Vector2(x, y));
-        st.AddVertex(new Vector3(x, 0, y));
+        st.AddUv(new Vector2(_wallUV.x1, _wallUV.y2));
+        st.AddVertex(new Vector3(x, 0, z));
 
         st.AddNormal(new Vector3(0, 0, 1));
-        st.AddUv(new Vector2(x, y + TileSize));
-        st.AddVertex(new Vector3(x + TileSize, TileSize, y));
+        st.AddUv(new Vector2(_wallUV.x2, _wallUV.y1));
+        st.AddVertex(new Vector3(x + TileSize, TileSize, z));
 
         st.AddNormal(new Vector3(0, 0, 1));
-        st.AddUv(new Vector2(x, y + TileSize));
-        st.AddVertex(new Vector3(x + TileSize, 0, y));
+        st.AddUv(new Vector2(_wallUV.x2, _wallUV.y2));
+        st.AddVertex(new Vector3(x + TileSize, 0, z));
     }
-    private void GenerateVerticalPlaneX(SurfaceTool st, int _x, int _y)
+    private void GenerateVerticalPlaneX(SurfaceTool st, int _x, int _z)
     {
         int x = _x * TileSize;
-        int y = _y * TileSize;
+        int z = _z * TileSize;
 
         // First triangle
         st.AddNormal(new Vector3(0, 0, 1));
-        st.AddUv(new Vector2(x, y));
-        st.AddVertex(new Vector3(x, 0, y));
+        st.AddUv(new Vector2(_wallUV.x1, _wallUV.y2));
+        st.AddVertex(new Vector3(x, 0, z));
 
         st.AddNormal(new Vector3(0, 0, 1));
-        st.AddUv(new Vector2(x, TileSize));
-        st.AddVertex(new Vector3(x, TileSize, y));
+        st.AddUv(new Vector2(_wallUV.x1, _wallUV.y1));
+        st.AddVertex(new Vector3(x, TileSize, z));
 
         st.AddNormal(new Vector3(0, 0, 1));
-        st.AddUv(new Vector2(x, y + TileSize));
-        st.AddVertex(new Vector3(x, TileSize, y + TileSize));
+        st.AddUv(new Vector2(_wallUV.x2, _wallUV.y1));
+        st.AddVertex(new Vector3(x, TileSize, z + TileSize));
 
         // Second triangle
         st.AddNormal(new Vector3(0, 0, 1));
-        st.AddUv(new Vector2(x, y));
-        st.AddVertex(new Vector3(x, 0, y));
+        st.AddUv(new Vector2(_wallUV.x1, _wallUV.y2));
+        st.AddVertex(new Vector3(x, 0, z));
 
         st.AddNormal(new Vector3(0, 0, 1));
-        st.AddUv(new Vector2(x, y + TileSize));
-        st.AddVertex(new Vector3(x, TileSize, y + TileSize));
+        st.AddUv(new Vector2(_wallUV.x2, _wallUV.y1));
+        st.AddVertex(new Vector3(x, TileSize, z + TileSize));
 
         st.AddNormal(new Vector3(0, 0, 1));
-        st.AddUv(new Vector2(x, y + TileSize));
-        st.AddVertex(new Vector3(x, 0, y + TileSize));
+        st.AddUv(new Vector2(_wallUV.x2, _wallUV.y2));
+        st.AddVertex(new Vector3(x, 0, z + TileSize));
     }
-    private void GenerateHorizontalPlane(SurfaceTool st, int _x, int _y)
+    private void GenerateHorizontalPlane(SurfaceTool st, int _x, int _z, int _y = 0)
     {
         int x = _x * TileSize;
+        int z = _z * TileSize;
         int y = _y * TileSize;
 
         // First triangle
         st.AddNormal(new Vector3(0, 0, 1));
-        st.AddUv(new Vector2(x, y));
-        st.AddVertex(new Vector3(x, 0, y));
+        st.AddUv(new Vector2(0, 0));
+        st.AddVertex(new Vector3(x, y, z));
 
         st.AddNormal(new Vector3(0, 0, 1));
-        st.AddUv(new Vector2(x + TileSize, y));
-        st.AddVertex(new Vector3(x + TileSize, 0, y));
+        st.AddUv(new Vector2(UVOffset, 0));
+        st.AddVertex(new Vector3(x + TileSize, y, z));
 
         st.AddNormal(new Vector3(0, 0, 1));
-        st.AddUv(new Vector2(x, y + TileSize));
-        st.AddVertex(new Vector3(x, 0, y + TileSize));
+        st.AddUv(new Vector2(0, UVOffset));
+        st.AddVertex(new Vector3(x, y, z + TileSize));
 
         // second triangle
         st.AddNormal(new Vector3(0, 0, 1));
-        st.AddUv(new Vector2(x + TileSize, y));
-        st.AddVertex(new Vector3(x + TileSize, 0, y));
+        st.AddUv(new Vector2(UVOffset, 0));
+        st.AddVertex(new Vector3(x + TileSize, y, z));
 
         st.AddNormal(new Vector3(0, 0, 1));
-        st.AddUv(new Vector2(x + TileSize, y + TileSize));
-        st.AddVertex(new Vector3(x + TileSize, 0, y + TileSize));
+        st.AddUv(new Vector2(UVOffset, UVOffset));
+        st.AddVertex(new Vector3(x + TileSize, y, z + TileSize));
 
         st.AddNormal(new Vector3(0, 0, 1));
-        st.AddUv(new Vector2(x, y + TileSize));
-        st.AddVertex(new Vector3(x, 0, y + TileSize));
+        st.AddUv(new Vector2(0, UVOffset));
+        st.AddVertex(new Vector3(x, y, z + TileSize));
     }
 
     public void SpawnPlayer(int _x, int _y)
@@ -174,15 +195,23 @@ public class DungeonGeneration3D : MeshInstance
         KinematicBody player = (KinematicBody)playerScene.Instance();
         Transform playerTransform = new Transform(player.Transform.basis, new Vector3(x, Transform.origin.y + 1, y));
         player.Transform = playerTransform;
+        player.Name = "Player";
         AddChild(player);
 
         _playerSpawned = true;
 
     }
-    private void _onRegenerateButtonPressed(int rooms, int mapSize)
+    private async void _onRegenerateButtonPressed(int rooms, int mapSize)
     {
         _roomCount = rooms;
 		_mapSize = mapSize;
+        var player = GetNode("Player");
+        player.QueueFree();
+        _playerSpawned = false;
+        var dungeon = GetNode("DungeonMesh");
+        dungeon.QueueFree();
+        await ToSignal(player, "tree_exited");
+        await ToSignal(dungeon, "tree_exited");
         Generate();
     }
 }
