e0edfe48b3534019ad0c51bcbba92f09ad291626
itaden1 <ethanshearer1@gmail.com>
2022-01-21 22:44:29 +1100

    rando walk

---
+++ Algorythms/RandomWalk.cs
@@ -0,0 +1,114 @@
+using System.Numerics;
+using System;
+using System.Collections.Generic;
+
+namespace GamePasta.DungeonAlgorythms
+{
+    public enum Direction
+    {
+        NORTH, SOUTH, EAST, WEST
+    }
+
+    public interface IRandomAlgorythm
+    {
+        List<Vector2> Execute();
+    }
+
+    public class RandomWalk : IRandomAlgorythm
+    {
+        private Vector2 _gridSize;
+        private Vector2 _start;
+        private List<Vector2> _mask;
+        private int _maxSteps;
+        private Direction _direction;
+        private Dictionary<Direction, Vector2> _directionMap;
+        public RandomWalk(Vector2 gridSize, Vector2 start, List<Vector2> mask, Direction direction, int maxSteps)
+        {
+            _gridSize = gridSize;
+            _start = start;
+            _mask = mask;
+            _maxSteps = maxSteps;
+            _direction = direction;
+            _directionMap = new Dictionary<Direction, Vector2>()
+            {
+                {Direction.NORTH, new Vector2(0, -1)},
+                {Direction.SOUTH, new Vector2(0, 1)},
+                {Direction.EAST, new Vector2(-1, 0)},
+                {Direction.WEST, new Vector2(1, 0)}
+            };
+        }
+        public List<Vector2> Execute()
+        {
+            Random rand = new Random();
+            Vector2 current = _start;
+            List<Vector2> path = new List<Vector2>();
+            int steps = 0;
+
+            while (true)
+            {
+                if (_maxSteps > 0 && steps >= _maxSteps) break;
+
+                List<Vector2> neighbours = new List<Vector2>();
+                path.Add(current);
+
+                foreach (Vector2 vec in GetPotentialNeighbours(current))
+                {
+                    if (NotOutOfBounds(vec) && !path.Contains(vec) && !_mask.Contains(vec) && NotInWrongDirection(vec, current))
+                    {
+                        neighbours.Add(vec);
+                    }
+                }
+
+                if (neighbours.Count <= 1)
+                {
+                    // We may be at the end of our path
+                    if (current.X + _directionMap[_direction].X > _gridSize.X &&
+                        current.Y + _directionMap[_direction].Y > _gridSize.Y)
+                    {
+                        // we signify the exit with a negative vec2
+                        neighbours.Add(new Vector2(-1, -1));
+                    }
+                }
+                if (neighbours.Count > 0)
+                {
+                    current = neighbours[rand.Next(0, neighbours.Count)];
+
+                    // check if exit
+                    if (current.X < 0 && current.Y < 0)
+                    {
+                        break;
+                    }
+                }
+                else break;
+                steps++;
+            }
+
+            return path;
+        }
+
+        private bool NotInWrongDirection(Vector2 vec, Vector2 current)
+        {
+            return (vec != new Vector2(current.X + _directionMap[_direction].X * -1, current.Y + _directionMap[_direction].Y * -1));
+        }
+
+        private bool NotOutOfBounds(Vector2 vec)
+        {
+            bool notUnderBounds = (vec.X >= 0 && vec.Y >= 0);
+            bool notOverbounds = (vec.X < _gridSize.X && vec.Y < _gridSize.Y);
+            return (notOverbounds && notUnderBounds);
+        }
+
+        private IEnumerable<Vector2> GetPotentialNeighbours(Vector2 current)
+        {
+            List<Vector2> neighbours = new List<Vector2>()
+            {
+                new Vector2(current.X, current.Y - 1),
+                new Vector2(current.X - 1, current.Y),
+                new Vector2(current.X, current.Y + 1),
+                new Vector2(current.X + 1, current.Y)
+            };
+            return neighbours;
+        }
+    }
+}
+
--- scripts/DungeonGeneration2D.cs
+++ scripts/DungeonGeneration2D.cs
@@ -1,58 +1,87 @@
 using Godot;
 using System;
-using ProcDungeon.Structures;
-using ProcDungeon.Algorythms;
-using ProcDungeon;
+using System.Collections.Generic;
+using GamePasta.DungeonAlgorythms;
+// using ProcDungeon.Structures;
+// using ProcDungeon.Algorythms;
+// using ProcDungeon;
+
 
 public class DungeonGeneration2D : TileMap
 {
-	[Export]
-	private int _mapSize = 40;
+    [Export]
+    private int _mapSize = 40;
 
-	[Export]
-	private int _roomCount = 20;
-	private enum _tileType {WALL, FLOOR}
-	private Random _random = new Random();
+    [Export]
+    private int _roomCount = 20;
+    private enum _tileType { WALL, FLOOR }
+    private Random _random = new Random();
 
-	public override void _Ready()
-	{
-		GD.Print("starting");
-		GetParent().GetNode("GUI").Connect("RegenerateDungeon", this, nameof(_onRegenerateButtonPressed));
-		Generate();
-	}
-	private void Generate()
-	{
-		Clear();
-		// Build a random dungeon
-		var alg = new BSPDungeonAlgorythm(_roomCount);
-		var myMap = new DungeonGrid(_mapSize, alg);
-		myMap.GenerateLayout();
+    public override void _Ready()
+    {
+        GD.Print("starting");
+        GetParent().GetNode("GUI").Connect("RegenerateDungeon", this, nameof(_onRegenerateButtonPressed));
+        Generate();
+    }
+    private void Generate()
+    {
+        Clear();
+        // Build a random dungeon
 
+        System.Numerics.Vector2 start = new System.Numerics.Vector2(0, (int)_mapSize / 2);
 
-		int[] tiles = new int[]{
-			TileSet.FindTileByName("wall_1"),
-			TileSet.FindTileByName("wall_2"),
-			TileSet.FindTileByName("wall_3"),
-		};
+        RandomWalk alg = new RandomWalk(
+            new System.Numerics.Vector2(_mapSize, _mapSize),
+            start,
+            new List<System.Numerics.Vector2>(),
+            Direction.WEST,
+            0
+        );
+        // var alg = new BSPDungeonAlgorythm(_roomCount);
+        // var myMap = new DungeonGrid(_mapSize, alg);
+        // myMap.GenerateLayout();
 
-		for(int y = 0; y < myMap.Grid.GetLength(0); y++)
-		{
-			for(int x = 0; x < myMap.Grid.GetLongLength(1); x++)
-			{
-				Tile t = myMap.Grid[y,x];
-				if (t.Blocking)
-				{
-					// place a wall tile
-					var tile = tiles[_random.Next(0, tiles.GetLength(0))];
-					SetCell(x, y, tile);
-				}
-			}
-		}
-	}
-	private void _onRegenerateButtonPressed(int rooms, int mapSize)
-	{
-		_roomCount = rooms;
-		_mapSize = mapSize;
-		Generate();
-	}
+        List<System.Numerics.Vector2> path = alg.Execute();
+        int[] tiles = new int[]{
+            TileSet.FindTileByName("wall_1"),
+            TileSet.FindTileByName("wall_2"),
+            TileSet.FindTileByName("wall_3"),
+        };
+
+        GD.Print(path.Count);
+        for (int i = 0; i < _mapSize - 1; i++)
+        {
+            for (int j = 0; j < _mapSize - 1; j++)
+            {
+                var tile = tiles[_random.Next(0, tiles.GetLength(0))];
+                SetCell(i, j, tile);
+
+            }
+        }
+        foreach (System.Numerics.Vector2 vec in path)
+        {
+            // var tile = tiles[_random.Next(0, tiles.GetLength(0))];
+            SetCell((int)vec.X, (int)vec.Y, -1);
+        }
+
+        // for(int y = 0; y < myMap.Grid.GetLength(0); y++)
+        // {
+        // 	for(int x = 0; x < myMap.Grid.GetLongLength(1); x++)
+        // 	{
+        // 		Tile t = myMap.Grid[y,x];
+        // 		if (t.Blocking)
+        // 		{
+        // 			// place a wall tile
+        // 			var tile = tiles[_random.Next(0, tiles.GetLength(0))];
+        // 			SetCell(x, y, tile);
+        // 		}
+        // 	}
+        // }
+    }
+    private void _onRegenerateButtonPressed(int rooms, int mapSize)
+    {
+        _roomCount = rooms;
+        _mapSize = mapSize;
+        Generate();
+    }
 }
